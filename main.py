da77a3019a01298d2d7993af2cbc086ec15ac9d3
laramos <l.a.ramos@amc.uva.nl>
2020-08-11 18:01:21 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 17:47:09 2020
+
+@author: laramos
+"""
+
+import numpy as np
+
+def compute_bmi(weight,height):
+    
+    bmi = weight/(height**2)
+    
+    return bmi
+
+
+
+
+
+    
+
